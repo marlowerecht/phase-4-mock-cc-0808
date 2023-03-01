b35fdbe520ae32bfb368ccf79d3518471f1ba338
Marlowe Recht <recht.marlowe@gmail.com>
2023-03-01 17:13:24 +0100

    finished deliverables i think

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,24 @@
+class CampersController < ApplicationController
+
+    def index
+        campers = Camper.all
+        render json: campers, status: :ok
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, status: :ok, include: [:activities]
+    end
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+
+end
